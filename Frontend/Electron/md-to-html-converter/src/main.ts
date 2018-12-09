6738d2e0d0eed1b08f263be0bb1de3075e19bfc5
Shekhar Nain <shnain@microsoft.com>
2018-12-09 16:43:24 +0530

    open file functionality added

--- Frontend/Electron/md-to-html-converter/src/main.ts
+++ Frontend/Electron/md-to-html-converter/src/main.ts
@@ -1,7 +1,12 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, dialog } from "electron";
+import * as fs from "fs";
 
+// This declared here so that mainRenderer will not get garbage collected,
+// once the ready callback finish executing, we want to mae it grbage collected once the window gets closed,
+// that's why added a even listener on 'close' as shown below.
+let mainRenderer: BrowserWindow;
 app.on("ready", () => {
-    const mainRenderer: BrowserWindow = new BrowserWindow({
+    mainRenderer = new BrowserWindow({
         title: "md-to-html",
         show: false // by default don't show the window until it's ready
     });
@@ -11,4 +16,22 @@ app.on("ready", () => {
     mainRenderer.once("ready-to-show", () => {
         mainRenderer.show();
     });
+
+    mainRenderer.on("close", () => {
+        mainRenderer = null;
+    })
 });
+
+export const getFileFromUserSelection = () => {
+    const files = dialog.showOpenDialog({
+        properties: ["openFile"],
+        filters: [
+            { name: "Markdown files", extensions: ["md", "markdown"] },
+            { name: "Text files", extensions: ["txt"] }
+        ]
+    });
+    if(!files) return;
+    const file = files[0];
+    const content = fs.readFileSync(file).toString();
+    return content;
+}
--- Frontend/Electron/md-to-html-converter/src/renderer.ts
+++ Frontend/Electron/md-to-html-converter/src/renderer.ts
@@ -1,1 +1,22 @@
-import  "./style.css";
+// import  "./style.css";
+import * as marked from "marked";
+import { remote } from "electron";
+const { getFileFromUserSelection } = remote.require("./main");
+
+const mdView = document.getElementById("markdown-view");
+const htmlView = document.getElementById("html-view");
+const openFileBtn = document.getElementById("open-file");
+
+const renderMdToHtml = (mdText: string) => {
+    htmlView.innerHTML = marked(mdText, {sanitize: true});
+}
+
+mdView.addEventListener("keyup", (event) => {
+    renderMdToHtml((event.target as any).value);
+});
+
+openFileBtn.addEventListener("click", () => {
+    const file = getFileFromUserSelection();
+    mdView.innerHTML = file;
+    renderMdToHtml(file);
+})
