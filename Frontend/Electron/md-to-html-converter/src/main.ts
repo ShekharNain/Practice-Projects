77c0fa8849b6a46b43e678e1dc6f68b88ab20214
Shekhar Nain <shnain@microsoft.com>
2018-12-13 11:50:47 +0530

    Features added:
    1. new file button will trigger a new window
    2. edited file closing should show up a warning dialogue box
    3.

--- Frontend/Electron/md-to-html-converter/src/main.ts
+++ Frontend/Electron/md-to-html-converter/src/main.ts
@@ -1,37 +1,76 @@
 import { app, BrowserWindow, dialog } from "electron";
 import * as fs from "fs";
 
-// This declared here so that mainRenderer will not get garbage collected,
+interface IFile {
+    path: string;
+    content: string;
+}
+
+// this declared here so that mainRenderer will not get garbage collected,
 // once the ready callback finish executing, we want to mae it grbage collected once the window gets closed,
 // that's why added a even listener on 'close' as shown below.
-let mainRenderer: BrowserWindow;
 app.on("ready", () => {
-    mainRenderer = new BrowserWindow({
-        title: "md-to-html",
-        show: false // by default don't show the window until it's ready
-    });
-    mainRenderer.loadURL(`file://${__dirname}/index.html`);
-
-    // show window once it is ready
-    mainRenderer.once("ready-to-show", () => {
-        mainRenderer.show();
-    });
-
-    mainRenderer.on("close", () => {
-        mainRenderer = null;
-    })
+    createWindow();
 });
 
-export const getFileFromUserSelection = () => {
-    const files = dialog.showOpenDialog({
+export function getFileFromUserSelection(targetWindow: BrowserWindow): string {
+    const files: string[] = dialog.showOpenDialog(targetWindow, {
         properties: ["openFile"],
         filters: [
             { name: "Markdown files", extensions: ["md", "markdown"] },
             { name: "Text files", extensions: ["txt"] }
         ]
     });
-    if(!files) return;
-    const file = files[0];
-    const content = fs.readFileSync(file).toString();
-    return content;
+    if(!files) { return; }
+    return files[0];
+}
+
+export function openFile(targetWindow: BrowserWindow, filePath?: string): IFile {
+    filePath = filePath || getFileFromUserSelection(targetWindow);
+    const content: string = fs.readFileSync(filePath).toString().replace(/\r\n/g, "\n");
+    console.log(content.charAt(15));
+    targetWindow.setTitle(`${filePath} - md2Html`);
+    targetWindow.webContents.send("file-opened", filePath, content);
+    targetWindow.setRepresentedFilename(filePath);
+    return { path: filePath, content };
+}
+
+const windows: Set<BrowserWindow> = new Set();
+
+export function createWindow(): void {
+    let newWindow: BrowserWindow = new BrowserWindow({
+        title: "md-to-html",
+        show: false // by default don't show the window until it's ready
+    });
+    windows.add(newWindow);
+    newWindow.loadURL(`file://${__dirname}/index.html`);
+
+    // show window once it is ready
+    newWindow.once("ready-to-show", () => {
+        newWindow.show();
+    });
+
+    // event triggered just before closing a window
+    newWindow.on("close", (event) => {
+        if (newWindow.isDocumentEdited()) {
+            event.preventDefault();
+            const result: number = dialog.showMessageBox(newWindow, {
+                type: "warning",
+                title: "Quit with unsaved changes",
+                message: "your changes will be lost if you don't save the changes",
+                buttons: [
+                    "Quit anyway",
+                    "cancel"
+                ],
+                defaultId: 0,
+                cancelId: 1
+            });
+            if(result === 0) { newWindow.destroy(); }
+        }
+    });
+
+    newWindow.on("closed", () => {
+        windows.delete(newWindow);
+        newWindow = null;
+    });
 }
--- Frontend/Electron/md-to-html-converter/src/renderer.ts
+++ Frontend/Electron/md-to-html-converter/src/renderer.ts
@@ -1,22 +1,64 @@
 // import  "./style.css";
 import * as marked from "marked";
-import { remote } from "electron";
-const { getFileFromUserSelection } = remote.require("./main");
+import { remote, BrowserWindow, ipcRenderer } from "electron";
 
-const mdView = document.getElementById("markdown-view");
-const htmlView = document.getElementById("html-view");
-const openFileBtn = document.getElementById("open-file");
+interface IFile {
+    path: string;
+    content: string;
+}
 
-const renderMdToHtml = (mdText: string) => {
+let openedFile: IFile = {
+    path: "",
+    content: ""
+};
+const { openFile, createWindow, IFile } = remote.require("./main");
+const currentWindow: BrowserWindow = remote.getCurrentWindow();
+
+const mdView: HTMLTextAreaElement = document.querySelector("#markdown-view");
+const htmlView: HTMLElement = document.getElementById("html-view");
+const openFileBtn: HTMLElement = document.getElementById("open-file");
+const newFileBtn: HTMLElement = document.getElementById("new-file");
+
+function renderMdToHtml(mdText: string): void {
     htmlView.innerHTML = marked(mdText, {sanitize: true});
 }
 
+function updateEditedState(isEdited: boolean): void {
+    currentWindow.setDocumentEdited(isEdited);
+    let title: string = "md2Html";
+    if (openedFile && openedFile.path) {
+        title = `${openedFile.path} - ${title}`;
+    }
+    if (isEdited) {
+        title = `${title} (Edited)`;
+    }
+    currentWindow.setTitle(title);
+}
+
 mdView.addEventListener("keyup", (event) => {
-    renderMdToHtml((event.target as any).value);
+    const currentContent: string = (event.target as any).value;
+    renderMdToHtml(currentContent);
+    updateEditedState(currentContent !== openedFile.content);
 });
 
 openFileBtn.addEventListener("click", () => {
-    const file = getFileFromUserSelection();
-    mdView.innerHTML = file;
-    renderMdToHtml(file);
-})
+    openFile(currentWindow);
+    // if (file) {
+    //     mdView.value = file.content;
+    //     renderMdToHtml(file.content);
+    //     openedFile = file;
+    // }
+});
+
+ipcRenderer.on("file-opened", (event: any, file: string, content: string) => {
+    openedFile.path = file;
+    openedFile.content = content;
+
+    mdView.value = content;
+    renderMdToHtml(content);
+    updateEditedState(false);
+});
+
+newFileBtn.addEventListener("click", () => {
+    createWindow();
+});
